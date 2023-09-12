535663a01af2eea97d8f4d3a7bdd638930170a47
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-09-13 00:45:14 +0530

    Final commit

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const apiKey ="aa58447215c9e4eac1da7622cf4e2fe0";
 const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchBoxx=document.querySelector(".search input");
 const searchBtn=document.querySelector(".search button");
-const weatherIcon=document.querySelector("weather-icon")
+const weatherIcon=document.querySelector(".weather-icon")
 async function checkWeather(city){
     const response = await fetch(apiUrl+city+`&appid=${apiKey}`);
+
+if(response.status==404){
+    document.querySelector(".error").style.display="block";
+    document.querySelector(".weather").style.display="none";
+
+}else{
+
     var data=await response.json();
-console.log(data);
+
 
 document.querySelector(".city").innerHTML=data.name;
 document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°C";
@@ -14,30 +21,33 @@ document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
 document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
 
 if(data.weather[0].main =="Clouds"){
-  weatherIcon.src=".images/clouds.png";
+  weatherIcon.src="images/clouds.png";
 
 }
 else if(data.weather[0].main =="Clear"){
-    weatherIcon.src=".images/clear.png";
+    weatherIcon.src="images/clear.png";
 
 }
 else if(data.weather[0].main =="Drizzle"){
-    weatherIcon.src=".images/drizzle.png";
+    weatherIcon.src="images/drizzle.png";
 }
 else if(data.weather[0].main =="Mist"){
-    weatherIcon.src=".images/mist.png";
+    weatherIcon.src="images/mist.png";
 }
 else if(data.weather[0].main =="Rain"){
-    weatherIcon.src=".images/rain.png";
+    weatherIcon.src="images/rain.png";
 }
 else if(data.weather[0].main =="Snow"){
-    weatherIcon.src=".images/snow.png";
-}
-else if(data.weather[0].main =="wind"){
-    weatherIcon.src=".images/wind.png";
+    weatherIcon.src="images/snow.png";
 }
 
+else if(data.weather[0].main =="Humidity"){
+    weatherIcon.src="images/humidity.png";
 }
+document.querySelector(".weather").style.display="block";
+document.querySelector(".error").style.display="none";
+
+}}
 searchBtn=addEventListener("click",()=>{
     checkWeather(searchBoxx.value);
 })
